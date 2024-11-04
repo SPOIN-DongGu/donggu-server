5ae456a03646ac2c7c1f467392ee56292dd7ae6e
ihtblwFIZ <vthyrachel@naver.com>
2024-11-04 14:07:51 +0900

    initial commit

---
+++ src/main/java/com/donggu/server/DongguApplication.java
@@ -0,0 +1,13 @@
+package com.donggu.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongguApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongguApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/donggu/server/DongguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donggu.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongguApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
