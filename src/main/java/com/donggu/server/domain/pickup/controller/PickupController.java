9bc52ea1da744a2c914dbd01c09d0e88dbb3f234
ihtblwFIZ <vthyrachel@naver.com>
2024-11-18 16:55:12 +0900

    docs: 스웨거에서 볼 api별 설명 추가

--- src/main/java/com/donggu/server/domain/pickup/controller/PickupController.java
+++ src/main/java/com/donggu/server/domain/pickup/controller/PickupController.java
@@ -3,6 +3,7 @@ package com.donggu.server.domain.pickup.controller;
 import com.donggu.server.domain.pickup.dto.PickupRequestDto;
 import com.donggu.server.domain.pickup.dto.PickupResponseDto;
 import com.donggu.server.domain.pickup.service.PickupService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,6 +21,7 @@ public class PickupController {
     // [관리자] 픽업 게임 등록
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/")
+    @Operation(summary = "픽업 게임 생성", description = "관리자 전용 기능: 게임 정보를 기재해 픽업 게임을 생성합니다")
     public ResponseEntity<Void> registerPickup(@RequestBody PickupRequestDto dto) {
         pickupService.registerPickup(dto);
         return ResponseEntity.ok().build();
@@ -28,6 +30,7 @@ public class PickupController {
     // [관리자] 픽업 게임 수정
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/{pickupId}")
+    @Operation(summary = "픽업 게임 수정", description = "관리자 전용 기능: 등록된 픽업 게임의 정보를 수정합니다")
     public ResponseEntity<Void> updatePickup(@PathVariable Long pickupId, @RequestBody PickupRequestDto dto) {
         pickupService.updatePickup(pickupId, dto);
         return ResponseEntity.ok().build();
@@ -36,6 +39,7 @@ public class PickupController {
     // [관리자] 픽업 게임 삭제
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{pickupId}")
+    @Operation(summary = "픽업 게임 삭제", description = "관리자 전용 기능: 등록된 픽업 게임을 삭제합니다")
     public ResponseEntity<Void> deletePickup(@PathVariable Long pickupId) {
         pickupService.deletePickup(pickupId);
         return ResponseEntity.ok().build();
@@ -43,12 +47,14 @@ public class PickupController {
 
     // [일반/관리자] 픽업 게임 전체 조회
     @GetMapping("/")
+    @Operation(summary = "픽업 게임 전체 조회", description = "전체 접근 기능: 등록된 픽업 게임 목록을 조회합니다")
     public ResponseEntity<List<PickupResponseDto>> getAllPickup() {
         return ResponseEntity.ok(pickupService.getAllPickup());
     }
 
     // [일반/관리자] 픽업 게임 게임별 조회
     @GetMapping("/{pickupId}")
+    @Operation(summary = "픽업 게임 상세 조회", description = "전체 접근 기능: 특정 픽업 게임의 상세 정보를 조회합니다")
     public ResponseEntity<PickupResponseDto> getPickup(@PathVariable Long pickupId) {
         return ResponseEntity.ok(pickupService.getPickup(pickupId));
     }
--- src/main/java/com/donggu/server/domain/pickupjoin/controller/PickupJoinController.java
+++ src/main/java/com/donggu/server/domain/pickupjoin/controller/PickupJoinController.java
@@ -4,6 +4,7 @@ import com.donggu.server.domain.pickupjoin.domain.Status;
 import com.donggu.server.domain.pickupjoin.dto.PickupJoinRequestDto;
 import com.donggu.server.domain.pickupjoin.dto.PickupJoinResponseDto;
 import com.donggu.server.domain.pickupjoin.service.PickupJoinService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,26 +19,26 @@ public class PickupJoinController {
 
     private final PickupJoinService pickupJoinService;
 
-    // 픽업 게임 신청과 거절을 관리
-    // 신청, (관리자)게임 별 신청 목록 조회, (관리자)거절
-
     // 픽업 게임 참여 신청
     @PostMapping("/")
+    @Operation(summary = "픽업 게임 신청", description = "회원 전용 기능: 신청자 정보를 기재해 픽업 게임을 신청합니다")
     public ResponseEntity<Void> applyPickup(@PathVariable Long pickupId, @RequestBody Long userId, @RequestBody PickupJoinRequestDto dto) {
         pickupJoinService.applyPickup(pickupId, userId, dto);
         return ResponseEntity.ok().build();
     }
 
-    // 픽업 게임 참여 신청 현황 게임별 조회
+    // [관리자] 픽업 게임 참여 신청 현황 게임별 조회
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/")
+    @Operation(summary = "픽업 게임 신청자 조회", description = "관리자 전용 기능: 픽업 게임별 신청자 목록을 조회합니다")
     public ResponseEntity<List<PickupJoinResponseDto>> getAppliedUsersByPickup(@PathVariable Long pickupId) {
         return ResponseEntity.ok(pickupJoinService.getAppliedUsersByPickup(pickupId));
     }
 
-    // 픽업 게임 참여 신청 승인/거절
+    // [관리자] 픽업 게임 참여 신청 승인/거절
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/{pickupUserId}")
+    @Operation(summary = "픽업 게임 신청 처리", description = "관리자 전용 기능: 참여 신청에 대해 승인 혹은 거절합니다")
     public ResponseEntity<Void> handleUserApply(@PathVariable Long pickupId, @PathVariable Long pickupUserId, @RequestBody Status status) {
         pickupJoinService.handleUserApply(pickupUserId, status);
         return ResponseEntity.ok().build();
