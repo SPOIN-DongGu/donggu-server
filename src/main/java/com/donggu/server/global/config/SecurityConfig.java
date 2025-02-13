4bb0ec833d0db55adb003efd4cee67e36ddf3963
ihtblwFIZ <vthyrachel@naver.com>
2025-02-13 16:40:52 +0900

    feat: cors 허용 origin 추가

--- src/main/java/com/donggu/server/global/config/SecurityConfig.java
+++ src/main/java/com/donggu/server/global/config/SecurityConfig.java
@@ -24,6 +24,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -70,6 +71,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080"));
+        configuration.setAllowedOriginPatterns(List.of("https://donggu-582cd.web.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Accept", "Authorization", "X-Real-IP"));
         configuration.setAllowCredentials(true);
